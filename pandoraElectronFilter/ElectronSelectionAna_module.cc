7ec2355e7c08c99f24ad4b660268219094ff2789
Wouter Van De Pontseele <wouter.vandepontseele@physics.ox.ac.uk>
2017-04-25 15:46:35 -0500

    filled some more tree parameters

--- pandoraElectronFilter/ElectronSelectionAna_module.cc
+++ pandoraElectronFilter/ElectronSelectionAna_module.cc
@@ -50,25 +50,32 @@ private:
   TFile*      fTFile;
   TTree*      fTree;
 
+
+  //Run Subrun Event 
+  Short_t    run;
+  Short_t    subrun;
+  Short_t    event;
+
   //Truth information
-  std::vector<Short_t>   mcevts_truth;                    ///< number of neutrino Interactions in the spill
-  std::vector<Short_t>   nuPDG_truth;                     ///< neutrino PDG code
-  std::vector<Short_t>   ccnc_truth;                      ///< 0=CC 1=NC
-  std::vector<Short_t>   mode_truth;                      ///< 0=QE/El, 1=RES, 2=DIS, 3=Coherent production
-  std::vector<Float_t>   enu_truth;                       ///< true neutrino energy
-  std::vector<Float_t>   nuvtxx_truth;                    ///< neutrino vertex x
-  std::vector<Float_t>   nuvtxy_truth;                    ///< neutrino vertex y
-  std::vector<Float_t>   nuvtxz_truth;                    ///< neutrino vertex z
+  std::vector<Short_t>   mcevts_truth;                  ///< number of neutrino Interactions in the spill
+  std::vector<Short_t>   nuPDG_truth;                   ///< neutrino PDG code
+  std::vector<Short_t>   ccnc_truth;                    ///< 0=CC 1=NC
+  std::vector<Short_t>   mode_truth;                    ///< 0=QE/El, 1=RES, 2=DIS, 3=Coherent production
+  std::vector<Float_t>   enu_truth;                     ///< true neutrino energy
+  std::vector<Float_t>   nuvtxx_truth;                  ///< neutrino vertex x
+  std::vector<Float_t>   nuvtxy_truth;                  ///< neutrino vertex y
+  std::vector<Float_t>   nuvtxz_truth;                  ///< neutrino vertex z
 
   //PandoraNu information
-  Short_t nnuvtx;                                       ///< Number of PandoraNu neutrino candidate vertices
+  Bool_t               passed;                          ///< Return true if there is at least one neutrino vertex that passed
+  Short_t              nnuvtx;                          ///< Number of PandoraNu neutrino candidate vertices
   std::vector<Float_t> nuvtxx;                          ///< x coordinate
   std::vector<Float_t> nuvtxy;                          ///< y coordinate
-  std::vector<Float_t> nuvtxz;                                 ///< z coordinate
+  std::vector<Float_t> nuvtxz;                          ///< z coordinate
   std::vector<Short_t> nuvtxpdg;                        ///< PDG code assigned by PandoraNu
-  std::vector<Float_t>center_of_charge_x;               ///< x Center of deposited charge
-  std::vector<Float_t>center_of_charge_y;               ///< y Center of deposited charge
-  std::vector<Float_t>center_of_charge_z;               ///< z Center of deposited charge
+  std::vector<Float_t> center_of_charge_x;              ///< x Center of deposited charge
+  std::vector<Float_t> center_of_charge_y;              ///< y Center of deposited charge
+  std::vector<Float_t> center_of_charge_z;              ///< z Center of deposited charge
 
   //Optical information
   Short_t nfls;                                         ///< Number of reconstructed flashes
@@ -93,6 +100,11 @@ lee::ElectronSelectionAna::ElectronSelectionAna(fhicl::ParameterSet const & pset
   fTFile = new TFile("FlashOutput.root", "RECREATE");
   fTree  = tfs->make<TTree>("flashtree","FlashAnalysis Tree");
 
+  //Set branches for (Run Subrun Event) 
+  fTree->Branch("run",     &run,     "run/S"       );
+  fTree->Branch("subrun",  &subrun,  "subrun/S"    );
+  fTree->Branch("event",   &event,   "event/S"     );
+
   //Set branches for truth information
   fTree->Branch("mcevts_truth", &mcevts_truth,   "mcevts_truth/S"               );
   fTree->Branch("nuPDG_truth",  &nuPDG_truth,    "nuPDG_truth[mcevts_truth]/S"  );
@@ -104,6 +116,7 @@ lee::ElectronSelectionAna::ElectronSelectionAna(fhicl::ParameterSet const & pset
   fTree->Branch("nuvtxz_truth", &nuvtxz_truth,   "nuvtxz_truth[mcevts_truth]/F" );
 
   //Set branches for PandoraNU information
+  fTree->Branch("passed",     &passed,     "passed/O"             );
   fTree->Branch("nnuvtx",     &nnuvtx,     "nnuvtx/S"             );
   fTree->Branch("nuvtxx",     &nuvtxx,     "nuvtxx[nnuvtx]/F"     );
   fTree->Branch("nuvtxy",     &nuvtxy,     "nuvtxy[nnuvtx]/F"     );
@@ -136,6 +149,7 @@ void lee::ElectronSelectionAna::analyze(art::Event const & e)
 {
   // Implementation of required member function here.
   fillTree(e);
+
   bool event_passed = fElectronEventSelectionAlg.eventSelected(e);
   if (event_passed){
     // Find out how many passing neutrino candidates there are:
@@ -149,11 +163,39 @@ void lee::ElectronSelectionAna::analyze(art::Event const & e)
 }
 void lee::ElectronSelectionAna::fillTree(art::Event const & e)
 {
+
+  // Fill run information 
+  run    = e.run(); 
+  subrun = e.subRun();
+  event  = e.event();
+
   // Fill truth information
   //TODO
 
   // Fill PandoraNu information
-  //TODO 
+  //TODO: PandoraNu PDG code of passed
+  passed = fElectronEventSelectionAlg.eventSelected(e);
+  if(passed)
+  {
+    // Find out how many passing neutrino candidates there are:
+    for (size_t i = 0; i < fElectronEventSelectionAlg.get_n_neutrino_candidates(); i ++)
+    {
+      if (fElectronEventSelectionAlg.get_neutrino_candidate_passed().at(i))
+      {
+        nnuvtx++;
+
+        TVector3 neutrino_vertex = fElectronEventSelectionAlg.get_neutrino_vertex().at(i);
+        nuvtxx.push_back(neutrino_vertex.X());
+        nuvtxy.push_back(neutrino_vertex.Y());
+        nuvtxz.push_back(neutrino_vertex.Z());
+
+        TVector3 center_of_charge = fElectronEventSelectionAlg.get_center_of_charge().at(i);
+        center_of_charge_x.push_back(center_of_charge.X());
+        center_of_charge_y.push_back(center_of_charge.Y());
+        center_of_charge_z.push_back(center_of_charge.Z());
+      }
+    }
+  }
 
   // Fill optical information
   art::InputTag optical_tag{"simpleFlashBeam"};
