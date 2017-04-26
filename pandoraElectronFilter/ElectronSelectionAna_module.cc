2916bb4dbffc169bec7eb9e73b979e78b396f759
Wouter Van De Pontseele <wouter.vandepontseele@physics.ox.ac.uk>
2017-04-26 12:12:11 -0500

    changed pfp index in fill pandorapart of tree

--- pandoraElectronFilter/ElectronSelectionAna_module.cc
+++ pandoraElectronFilter/ElectronSelectionAna_module.cc
@@ -212,21 +212,22 @@ void lee::ElectronSelectionAna::fillTree(art::Event const & e)
     // Find out how many passing neutrino candidates there are:
     for (size_t i = 0; i < fElectronEventSelectionAlg.get_n_neutrino_candidates(); i ++)
     {
-      if (fElectronEventSelectionAlg.get_neutrino_candidate_passed().at(i))
+      size_t pfpindex = fElectronEventSelectionAlg.get_primary_indexes().at(i);
+      if (fElectronEventSelectionAlg.get_neutrino_candidate_passed().at(pfpindex))
       {
         nnuvtx++;
 
-        TVector3 neutrino_vertex = fElectronEventSelectionAlg.get_neutrino_vertex().at(i);
+        TVector3 neutrino_vertex = fElectronEventSelectionAlg.get_neutrino_vertex().at(pfpindex);
         nuvtxx.push_back(neutrino_vertex.X());
         nuvtxy.push_back(neutrino_vertex.Y());
         nuvtxz.push_back(neutrino_vertex.Z());
 
-        TVector3 center_of_charge = fElectronEventSelectionAlg.get_center_of_charge().at(i);
+        TVector3 center_of_charge = fElectronEventSelectionAlg.get_center_of_charge().at(pfpindex);
         center_of_charge_x.push_back(center_of_charge.X());
         center_of_charge_y.push_back(center_of_charge.Y());
         center_of_charge_z.push_back(center_of_charge.Z());
 
-        recob::PFParticle const& pfp = pfparticle_handle->at(fElectronEventSelectionAlg.get_primary_indexes()[i]);
+        recob::PFParticle const& pfp = pfparticle_handle->at(pfpindex);
         nuvtxpdg.push_back(pfp.PdgCode());
       }
     }
